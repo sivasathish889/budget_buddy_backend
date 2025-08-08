0125c67b52d6ce4600aff2c5f863b68e1fefe153
sivasathish889 <sivasathish889@gmail.com>
2025-08-08 15:41:46 +0530

    project init

---
+++ src/test/java/com/budgetBuddy/budget_buddy_app/BudgetBuddyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetBuddy.budget_buddy_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBuddyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
