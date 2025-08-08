cc790fe0c6fd257f54b4bf193bf01f46088d2bfa
shiva9342 <clgshiva365@gmail.com>
2025-08-08 16:06:18 +0530

    name changed

---
+++ src/main/java/com/backend/budget_buddy/BudgetBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.backend.budget_buddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/budget_buddy/BudgetBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.budget_buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
